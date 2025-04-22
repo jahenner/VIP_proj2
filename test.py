3ced1ea36fcde0818f9b69a7798a6abddfdb9eeb
Alex H <jahenner@gmail.com>
2025-04-22 12:44:33 -0400

    Updated test.py to run on single file

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ import os
 
 def main():
     os.chdir("./GEOSmie")
-    filename = os.path.join(os.getcwd(), "generate_geos_optics.sh")
-    print(filename)
-    test_command = ["/usr/bin/bash", filename]
-    subprocess.run(test_command, capture_output=True, text=True, check=True)
+    test_command = ["python", "runoptics.py", "--name", "./geosparticles/bc.json"]
+    subprocess.run(test_command, text=True, check=True)
+    test_command = ["python", "runbands.py", "--filename", "./optics_bc.nc4"]
+    subprocess.run(test_command, text=True, check=True)
 
 
 if __name__ == "__main__":
@@ -20,12 +20,11 @@ if __name__ == "__main__":
     tracemalloc.start()
     main()
     profiler.disable()
-    tracemalloc.stop()
     stats = pstats.Stats(profiler).sort_stats('cumulative')
     stats.print_stats(10)
-    
-    print()
+
     print('-'*40)
     current, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
     print(f"Current memory usage: {current / 1024:.2f} KiB")
     print(f"Peak memory usage: {peak / 1024:.2f} KiB")
